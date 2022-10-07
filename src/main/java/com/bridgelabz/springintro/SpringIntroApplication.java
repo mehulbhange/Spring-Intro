e573649e7fd56832045de0855fb61d1afb9e90c8
mehulbhange <bhangemehul@gmail.com>
2022-10-07 15:18:55 +0530

    ADDED:[Mehul] hello message in spring boot application

---
+++ src/main/java/com/bridgelabz/springintro/SpringIntroApplication.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.springintro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIntroApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello from Bridgelabz");
+        SpringApplication.run(SpringIntroApplication.class, args);
+    }
+
+}
